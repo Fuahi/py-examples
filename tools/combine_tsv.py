1cee2aac6bd23dd7db958dab40e00330516dccf6
Frank Lee <frl162@clourneysemi.com>
2023-12-20 09:39:22 +0800

    增加处理文档和文件夹的工具

---
+++ tools/combine_tsv.py
@@ -0,0 +1,26 @@
+import os
+
+
+def merge_tsv_files(directory, output_file):
+    with open(output_file, "w") as outfile:
+        for filename in os.listdir(directory):
+            if filename.endswith(".tsv"):
+                filepath = os.path.join(directory, filename)
+                with open(filepath, "r") as infile:
+                    lines = infile.readlines()
+                    outfile.writelines(lines[1:])  # 从第二行开始写入到输出文件
+
+
+def list_all_file(directory, output_file):
+    with open(output_file, "w") as outfile:
+        for filename in os.listdir(directory):
+            outfile.writelines(filename + "\n")  # 写入到输出文件
+
+
+# 指定目录和输出文件路径
+directory = rf"\\10.30.9.254\share\Frank\FBIQCal_Deal"
+output_file = rf"\\10.30.9.254\share\Frank\FBIQCal_Deal\allNTboot.tsv"
+
+# 调用函数进行合并
+merge_tsv_files(directory, output_file)
+#list_all_file(directory, output_file)
---
+++ tools/rename.py
@@ -0,0 +1,13 @@
+import os
+
+source_dir = rf'D:/AFE-Bench0/rename'
+old_str = 'CyCTTNV'
+new_str = 'CyCLTNV'
+
+
+for filename in os.listdir(source_dir):
+    if os.path.isfile(os.path.join(source_dir, filename)):
+        new_filename = filename.replace(old_str, new_str)
+        old_fullpath = os.path.join(source_dir,filename)
+        new_fullpath = os.path.join(source_dir,new_filename)
+        os.rename(old_fullpath,new_fullpath)
